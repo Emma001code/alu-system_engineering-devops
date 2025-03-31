612789aa0eda24c8c01b54b8b875976451a1f497
Emmanuel <e.ngwoke@alustudent.com>
2025-03-31 17:57:04 +0200

    Add changes

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,6 @@
 # A puppet that increases the ULIMIT of nginx default from 15 to 4096
 
-#increase ULIMIT
-exec { 'increase-ulimit':
-command => '/bin/sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx',
-}
-
-#restart nginx
-exec { 'restart-nginx':
-command => '/usr/sbin/service nginx restart'
+exec { 'increase user-limit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx; sudo service nginx restart',
+  path    => '/usr/bin/:/usr/sbin:/bin/'
 }
