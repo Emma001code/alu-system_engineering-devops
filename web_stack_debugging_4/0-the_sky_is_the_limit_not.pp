79bee861f5bfc048be7052ea1153cba19565c25f
Emmanuel <e.ngwoke@alustudent.com>
2025-03-31 17:29:40 +0200

    webstack 4

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,11 @@
-# Increase ULIMIT of nginx default from 15 to 4096
-exec { 'increase_ulimit':
-  command => 'sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx',
-  path    => ['/bin', '/usr/bin'],
-  onlyif  => 'grep \'ULIMIT="-n 15"\' /etc/default/nginx',
+# A puppet that increases the ULIMIT of nginx default from 15 to 4096
+
+#increase ULIMIT
+exec { 'increase-ulimit':
+command => '/bin/sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx',
 }
 
-# Restart nginx
-exec { 'restart_nginx':
-  command     => 'service nginx restart',
-  path        => ['/usr/sbin', '/sbin', '/bin', '/usr/bin'],
-  refreshonly => true,
+#restart nginx
+exec { 'restart-nginx':
+command => '/usr/sbin/service nginx restart'
 }
